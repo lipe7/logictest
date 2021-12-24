73a2fcafb6a79f180043daf11b21589a62c0f33a
Felipe Campos <felipec165@gmail.com>
2021-12-23 23:14:17 -0300

    teste de logica

--- src/ProductStructure.php
+++ src/ProductStructure.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace App;
 
 class ProductStructure
@@ -20,7 +21,32 @@ class ProductStructure
 
     public function make(): array
     {
-       //todo your code.
-        return [];
+        $products = ProductStructure::products;
+
+        $newA = [];
+        foreach ($products as $value) {
+            $value = explode('-', $value, 2);
+            $color = $value[0];
+            $size = $value[1];
+
+            /*----------------------------------------------------------------
+                1. VERIFICA SE EXISTE UMA COR COMO KEY NO ARRAY PRINCIPAL
+            CASO NÃO EXISTA CRIA UMA KEY.
+                2. CASO JÁ EXISTA É VERIFICADO SE JÁ EXISTE O TAMANHO NO ARRAY
+            CASO EXISTA É INCREMENTADO 1, CASO CONTRARIO GERA UM NOVO.
+            ----------------------------------------------------------------*/
+
+            if (!array_key_exists($color, $newA)) {
+                $newA[$color] = [$size => 1];
+            } else {
+                if (!array_key_exists($size, $newA[$color])) {
+                    $newA[$color][$size] = 1;
+                } else {
+                    $newA[$color][$size]++;
+                }
+            }
+        }
+
+        return $newA;
     }
 }
